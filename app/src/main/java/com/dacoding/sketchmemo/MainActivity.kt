87046e530d0b12904d0acc3398ea0e492ba02a27
Denny <denisandreev1803@gmail.com>
2023-09-18 21:16:17 +0300

    Initial commit

---
+++ app/src/main/java/com/dacoding/sketchmemo/MainActivity.kt
@@ -0,0 +1,28 @@
+package com.dacoding.sketchmemo
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.dacoding.sketchmemo.ui.theme.SketchMemoTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            SketchMemoTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    TODO()
+                }
+            }
+        }
+    }
+}
+
